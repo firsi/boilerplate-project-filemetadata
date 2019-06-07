f7979815b6c2ccf2c8e3d629b02830bd044aac97
Alex <43517259+firsi@users.noreply.github.com>
2019-06-07 22:41:30 +0000

    file uploading

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var express = require('express');
 var cors = require('cors');
 
 // require and use "multer"...
+var multer = require('multer');
+var upload = multer();
 
 var app = express();
 
@@ -18,6 +20,24 @@ app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
 });
 
+
+
+
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res, next) => {
+    const file = req.file;
+    
+    if(!file){
+      const error = new Error('please upload a file');
+      error.httpStatusCode = 400;
+      return next(error);
+    }
+  
+    res.json({name:file.originalname,
+              type: file.mimetype,
+              size:file.size
+             });
+})
 app.listen(process.env.PORT || 3000, function () {
   console.log('Node.js listening ...');
 });
